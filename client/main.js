5cc14e19c730ef4e4506c49419f4124ac3a79ddc
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-02-24 13:36:13 -0500

    initializes vue instance

---
+++ client/main.js
@@ -0,0 +1,10 @@
+// create new Vue instance
+// el: id of main Vue div
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'For warm feet'
+    }
+})
