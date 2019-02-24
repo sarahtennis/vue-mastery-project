37f163f6fa1e7f7008379570ff0c5794e1ca772f
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-02-24 15:55:44 -0500

    conditionally renders out of stock view of variant using computed properties

--- client/main.js
+++ client/main.js
@@ -4,21 +4,23 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
         description: 'For warm feet',
-        image: './green-white.jpg',
-        inventory: 100,
+        selectedVariant: 0,
         details: ["80% cotton", "20% polyester", "Gender-netural"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
-                variantImage: 'green-white.jpg'
+                variantImage: 'green-white.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
-                variantImage: 'blue-white.jpg'
+                variantImage: 'blue-white.jpg',
+                variantQuantity: 0
             }
         ],
         cart: 0
@@ -27,8 +29,19 @@ var app = new Vue({
         addToCart: function () {
             this.cart += 1
         },
-        updateProduct: function (variantImage) {
-            this.image = variantImage
+        updateProduct: function (index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title: function () {
+            return this.brand + ' ' + this.product
+        },
+        image: function () {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock: function () {
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
 })
